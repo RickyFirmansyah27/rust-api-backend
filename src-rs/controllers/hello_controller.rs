a2eeffe9954408102c354e0ee10edec1cea61182
ricky firmansyah <rickyfirmansyah27@gmail>
2025-04-11 11:28:06 +0700

    implemented logger

--- src-rs/controllers/hello_controller.rs
+++ src-rs/controllers/hello_controller.rs
@@ -1,7 +1,9 @@
 use vercel_runtime::{Body, Error, Request, Response};
 use crate::helpers::base_response;
+use crate::helpers::logger;
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    logger::info_msg("[HelloController] - Info");
     let message = "Hello from Rust Service";
     base_response::success(message, None)
 }
--- src-rs/controllers/user_controller.rs
+++ src-rs/controllers/user_controller.rs
@@ -1,6 +1,8 @@
 use vercel_runtime::{Body, Error, Request, Response};
 use crate::helpers::base_response;
+use crate::helpers::logger;
 
 pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    logger::info_msg("[UserController] - Info");
     base_response::success("User created successfully", None)
 }
